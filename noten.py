c51683b3f7a23e66e792e5dea63cc0f0129b93dd
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2023-11-28 10:57:21 +0100

    my first repository

---
+++ noten.py
@@ -0,0 +1,15 @@
+studenten = [
+    {'name': 'Max', 'noten': [85, 90, 92]},
+    {'name': 'Lisa', 'noten': [88, 87, 90]},
+    {'name': 'Kerstin', 'noten': [78, 85, 80]}
+]
+
+def durchschnitt_berechnen(noten):
+    # Code hier schreiben
+   return sum(noten) / len(noten) 
+
+# Test
+for student in studenten:
+    durchschnitt = durchschnitt_berechnen(student['noten'])
+    print(f"{student['name']}: Durchschnitt =", round(durchschnitt,2))
+
